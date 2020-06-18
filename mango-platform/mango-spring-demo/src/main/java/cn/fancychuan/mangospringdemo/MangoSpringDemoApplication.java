3986e65d6c27bc6470fcd07bc2f995128281e88d
hp-fancy <1247375074@qq.com>
2020-06-18 23:43:30 +0800

    集成MyBatis

--- mango-platform/mango-spring-demo/src/main/java/cn/fancychuan/mangospringdemo/MangoSpringDemoApplication.java
+++ mango-platform/mango-spring-demo/src/main/java/cn/fancychuan/mangospringdemo/MangoSpringDemoApplication.java
@@ -3,7 +3,7 @@ package cn.fancychuan.mangospringdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cn.fancychuan.mangospringdemo"})
 public class MangoSpringDemoApplication {
 
     public static void main(String[] args) {
