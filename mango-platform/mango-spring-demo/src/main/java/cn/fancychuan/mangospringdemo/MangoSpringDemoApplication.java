d52419c9ff21ef27af5c020b7ce096b0c2c3b1f1
hp-fancy <1247375074@qq.com>
2020-06-18 22:50:11 +0800

    springboot初始化

---
+++ mango-platform/mango-spring-demo/src/main/java/cn/fancychuan/mangospringdemo/MangoSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.fancychuan.mangospringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoSpringDemoApplication.class, args);
+    }
+
+}
---
+++ mango-platform/mango-spring-demo/src/test/java/cn/fancychuan/mangospringdemo/MangoSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.fancychuan.mangospringdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MangoSpringDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
